5f99d4e549946801c5fd6556f8c758103deb6d07
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-10-12 16:46:46 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tribal",
+    version="0.1",
+    packages=find_packages(),
+    # Optional metadata
+    author="James Stevenson",
+    author_email="opensource@jamesstevenson.me",
+    description="ML for countering violent extremism",
+    url="https://github.com/CartographerLabs/Tribal",
+)
