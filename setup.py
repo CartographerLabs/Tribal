1e2cb6b4b1f178c9265d5a0adaa74b7d5dd0f4a7
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-10-12 17:51:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup, find_packages
 
+# Read the requirements from the requirements.txt file
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="Tribal",
-    version="0.9",
+    version="0.10",
     packages=find_packages(),
     # Optional metadata
     author="James Stevenson",
     author_email="opensource@jamesstevenson.me",
     description="ML for countering violent extremism",
     url="https://github.com/CartographerLabs/Tribal",
+    install_requires=requirements,  # Dependencies from requirements.txt
 )
