534e9b16d2fd62e76d4504cbb041db994ff65baf
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-10-12 17:21:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Tribal",
-    version="0.6",
+    version="0.7",
     packages=find_packages(),
     # Optional metadata
     author="James Stevenson",
