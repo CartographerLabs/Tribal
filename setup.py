bbd13ed457614e29f3ce682e546af19eded3b2c5
James Stevenson <28961606+user1342@users.noreply.github.com>
2024-10-14 22:22:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name="Tribal",
-    version="0.18",
+    version="0.19",
     packages=find_packages(),
     # Optional metadata
     author="James Stevenson",
