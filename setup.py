d5493748767746da76e8faaa9a1dd40c5d445e36
James Stevenson <28961606+user1342@users.noreply.github.com>
2025-01-02 11:07:27 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-# Read the requirements from requirements.txt
-# Make sure your requirements.txt references the Git-based package with a valid specifier, e.g.:
-#   easyLLM @ git+https://github.com/user1342/easyLLM.git#egg=easyLLM
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = [
         line.strip()
@@ -16,22 +13,11 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="tribal",
-    version="0.1.1",
+    version="0.1.2",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/user1342/tribal",
     packages=find_packages(),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    python_requires=">=3.8",
     install_requires=requirements,
     entry_points={
         "console_scripts": [
